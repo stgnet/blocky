d75c6aaaed0eee15e19ab55b67280d4bcbdeea02
Nicolas Martin <nicolas.martin.0986@gmail.com>
2020-11-09 17:42:45 -0500

    Resolver (#5)
    * fix fqdn
    
    * fix rr_header type

--- resolver/cname_resolver.go
+++ resolver/cname_resolver.go
@@ -22,16 +22,16 @@ func NewCnameResolver(cfg config.CnameConfig) ChainedResolver {
 }
 
 // Configuration returns the string representation of the configuration
-func (rr *CnameResolver) Configuration() (result []string) {
+func (cr *CnameResolver) Configuration() (result []string) {
 
-	for k, val := range rr.cfg.Groups {
+	for k, val := range cr.cfg.Groups {
 		result = append(result, fmt.Sprintf("group %s redirects to %s", k, val.Cname))
 		for _, v := range val.Domains {
 			result = append(result, fmt.Sprintf("domain %s", v))
 		}
 	}
 
-	for key, val := range rr.cfg.ClientGroupsBlock {
+	for key, val := range cr.cfg.ClientGroupsBlock {
 		result = append(result, fmt.Sprintf("  %s = \"%s\"", key, strings.Join(val, ";")))
 	}
 
@@ -39,35 +39,39 @@ func (rr *CnameResolver) Configuration() (result []string) {
 }
 
 // Resolve requested domain and looks if it's part of any restriction
-func (rr *CnameResolver) Resolve(req *Request) (*Response, error) {
+func (cr *CnameResolver) Resolve(req *Request) (*Response, error) {
 	logger := withPrefix(req.Log, "cname_resolver")
 
 	for _, question := range req.Req.Question {
 		domain := util.ExtractDomain(question)
-		groups := rr.groupsToCheckForClient(req)
+		groups := cr.groupsToCheckForClient(req)
 		if len(groups) <= 0 {
 			continue
 		}
 
 		if len(domain) > 0 {
 			for _, g := range groups {
-				for _, d := range rr.cfg.Groups[g].Domains {
+				for _, d := range cr.cfg.Groups[g].Domains {
 					if d == domain {
 						response := new(dns.Msg)
 						response.SetReply(req.Req)
 
-						dnsCnameReq := new(dns.CNAME)
-						h := dns.RR_Header{Name: question.Name, Rrtype: question.Qtype, Class: dns.ClassINET, Ttl: customDNSTTL}
+						rr := new(dns.CNAME)
+						h := dns.RR_Header{
+							Name:   question.Name,
+							Rrtype: dns.TypeCNAME,
+							Class:  dns.ClassINET,
+							Ttl:    customDNSTTL,
+						}
+						rr.Target = cr.cfg.Groups[g].Cname
+						rr.Hdr = h
 
-						dnsCnameReq.Target = rr.cfg.Groups[g].Cname
-						dnsCnameReq.Hdr = h
-
-						response.Answer = append(response.Answer, dnsCnameReq)
+						response.Answer = append(response.Answer, rr)
 
 						logger.WithFields(logrus.Fields{
 							"answer": util.AnswerToString(response.Answer),
 							"domain": domain,
-						}).Debugf("returning restricted dns entry")
+						}).Debug("returning restricted dns entry")
 
 						return &Response{Res: response, RType: CUSTOMDNS, Reason: "RESTRICTED DNS"}, nil
 					}
@@ -75,18 +79,18 @@ func (rr *CnameResolver) Resolve(req *Request) (*Response, error) {
 			}
 		}
 	}
-	return rr.next.Resolve(req)
+	return cr.next.Resolve(req)
 }
 
-func (rr *CnameResolver) groupsToCheckForClient(request *Request) (groups []string) {
+func (cr *CnameResolver) groupsToCheckForClient(request *Request) (groups []string) {
 	// try client names
-	groupsByName, found := rr.cfg.ClientGroupsBlock[request.ClientIP.String()]
+	groupsByName, found := cr.cfg.ClientGroupsBlock[request.ClientIP.String()]
 	if found {
 		groups = append(groups, groupsByName...)
 	}
 
 	if len(groups) == 0 {
-		groups = rr.cfg.ClientGroupsBlock["default"]
+		groups = cr.cfg.ClientGroupsBlock["default"]
 	}
 
 	sort.Strings(groups)
--- resolver/cname_resolver_test.go
+++ resolver/cname_resolver_test.go
@@ -22,7 +22,7 @@ func TestCnameResolver_Resolve(t *testing.T) {
 		Groups: map[string]config.Groups{
 			"youtube": {
 				Domains: []string{"youtube.com", "other.youtube.com"},
-				Cname:   "restrict.youtube.com",
+				Cname:   "restrict.youtube.com.",
 			},
 		},
 		ClientGroupsBlock: map[string][]string{
@@ -34,31 +34,33 @@ func TestCnameResolver_Resolve(t *testing.T) {
 	sut.Next(m)
 
 	// Client in block, querying restricted domain. Respond with domain restriction
-	resp, err = sut.Resolve(newRequestWithClient("other.youtube.com", dns.TypeA, "1.2.1.2", "unknown"))
+	resp, err = sut.Resolve(newRequestWithClient("other.youtube.com.", dns.TypeA, "1.2.1.2", "unknown"))
 	assert.Nil(t, err)
-	b := BeDNSRecord("other.youtube.com", dns.TypeA, 3600, "restrict.youtube.com")
+	b := BeDNSRecord("other.youtube.com.", dns.TypeA, 3600, "restrict.youtube.com.")
 	ok, err := b.Match(resp.Res.Answer)
 	assert.Nil(t, err)
 	assert.True(t, ok)
 	assert.Equal(t, CUSTOMDNS, resp.RType)
 
 	// Client in block, querying alternate restricted domain. Respond with domain restriction
-	resp, err = sut.Resolve(newRequestWithClient("youtube.com", dns.TypeA, "1.2.1.2", "unknown"))
+	resp, err = sut.Resolve(newRequestWithClient("youtube.com.", dns.TypeA, "1.2.1.2", "unknown"))
 	assert.Nil(t, err)
-	b = BeDNSRecord("youtube.com", dns.TypeA, 3600, "restrict.youtube.com")
+	b = BeDNSRecord("youtube.com.", dns.TypeA, 3600, "restrict.youtube.com.")
 	ok, err = b.Match(resp.Res.Answer)
 	assert.Nil(t, err)
 	assert.True(t, ok)
 	assert.Equal(t, CUSTOMDNS, resp.RType)
 
 	// Client in block. Querying restricted domain. Respond without restriction
-	resp, err = sut.Resolve(newRequestWithClient("starcraft.com", dns.TypeA, "1.2.1.2", "unknown"))
+	resp, err = sut.Resolve(newRequestWithClient("starcraft.com.", dns.TypeA, "1.2.1.2", "unknown"))
 	assert.Nil(t, err)
+	assert.Nil(t, resp.Res.Answer)
 	// was delegated to the next resolver
 
 	// Client not in block. Querying restricted domain. Respond without restriction
-	resp, err = sut.Resolve(newRequestWithClient("youtube.com", dns.TypeA, "1.2.1.3", "unknown"))
+	resp, err = sut.Resolve(newRequestWithClient("youtube.com.", dns.TypeA, "1.2.1.3", "unknown"))
 	assert.Nil(t, err)
+	assert.Nil(t, resp.Res.Answer)
 	// was delegated to the next resolver
 
 }
